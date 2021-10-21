7bf7c925413bd0f57e5d402ddf938e563105cbbb
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2021-10-21 01:26:31 -0400

    added new output

--- main.js
+++ main.js
@@ -3,4 +3,5 @@ import { selectRandomWordFromWords, getRandomNumber } from './functions.js';
 
 console.log("\nRolling for a randomized username...\n");
 console.log(selectRandomWordFromWords(words) + selectRandomWordFromWords(words) + getRandomNumber(9999));
+console.log(selectRandomWordFromWords(words) + selectRandomWordFromWords(words) + getRandomNumber(99));
 
