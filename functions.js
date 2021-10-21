20216e542dd3a7d7e7ab9ba581a004edf6efa270
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2021-10-21 00:59:59 -0400

    added comments

--- functions.js
+++ functions.js
@@ -1,7 +1,9 @@
+// use an array of words, like 'word' from 'wordList.js'
 function selectRandomWordFromWords(array) {
     return array[getRandomNumber(array.length)];
 }
 
+// pass in the maximum number as 'num'
 function getRandomNumber(num) {
     return Math.floor((Math.random() * num));
 }
