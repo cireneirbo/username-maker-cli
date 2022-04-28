2d290f410c57e8c77ad01617cef8c4e1d11983b3
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2022-04-28 12:34:57 -0400

    Randomized better and added a timeout delay

--- main.js
+++ main.js
@@ -1,7 +1,50 @@
 import { words } from './wordList.js';
 import { selectRandomWordFromWords, getRandomNumber } from './functions.js';
 
+
+function randomizedUsernameCombination(num) {
+  let newPassword = "";
+  const comboOptions = [selectRandomWordFromWords(words), selectRandomWordFromWords(words), getRandomNumber(9), getRandomNumber(99), getRandomNumber(999), getRandomNumber(9999)];
+  switch(num) {
+    case 0:
+      newPassword = console.log(comboOptions[0] + comboOptions[5] + comboOptions[1]);
+      break;
+    case 1:
+      newPassword = console.log(comboOptions[4] + comboOptions[3] + comboOptions[0]);
+      break;
+    case 2:
+      newPassword = console.log(comboOptions[0] + comboOptions[4] + comboOptions[1]);
+      break;
+    case 3:
+      newPassword = console.log(comboOptions[4] + comboOptions[0] + comboOptions[4]);
+      break;
+    case 4:
+      newPassword = console.log(comboOptions[1] + comboOptions[2] + comboOptions[0]);
+      break;
+    case 5:
+      newPassword = console.log(comboOptions[0] + comboOptions[3] + comboOptions[1]);
+      break;
+    case 6:
+      newPassword = console.log(comboOptions[0] + comboOptions[2] + comboOptions[1]);
+      break;
+    case 7:
+      newPassword = console.log(comboOptions[2] + comboOptions[1] + comboOptions[0]);
+      break;
+    case 8:
+      newPassword = console.log(comboOptions[3] + comboOptions[0] + comboOptions[5]);
+      break;
+    default: "Something went wrong!";
+  }  
+  return newPassword;
+}
+
+
+
 console.log("\nRolling for a randomized username...\n");
-console.log(selectRandomWordFromWords(words) + selectRandomWordFromWords(words) + getRandomNumber(9999));
-console.log(selectRandomWordFromWords(words) + selectRandomWordFromWords(words) + getRandomNumber(99));
+
+
+setTimeout(()=> {
+  randomizedUsernameCombination(getRandomNumber(9));
+}, 1000);
+
 
